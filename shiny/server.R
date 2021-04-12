c9406b0762fe03a6f3e37dcc9a0c29d13e236b8d
chelmann <chelmann@psrc.org>
2021-04-12 13:09:59 -0700

    tweak to ui for income

--- shiny/server.R
+++ shiny/server.R
@@ -20,7 +20,7 @@ shinyServer(function(input, output) {
     output$table_county_education_bs <- renderDataTable({create.clean.tbl(yr=latest.yr, g.type=c("County"), c.name="Bachelor's degree or higher", t.container=create.custom.container("County","Bachelor's degree or higher"), t.cols=county.tot.cols, s.cols=county.shr.cols, e.type=input$EducationType)})
     
     # Median Income Charts and Tables
-    output$chart_region_income <- renderPlotly({create.bar.chart.facet(yr=latest.yr, g.type=c("MSA"), e.type=input$IncomeType, c.name="Median income", c.facet=2, w.label=scales::dollar_format(),w.pre="$")})
+    output$chart_region_income <- renderPlotly({create.bar.chart.facet(yr=latest.yr, g.type=c("MSA"), e.type=input$IncomeType, c.name="Median income", c.facet=1, w.label=scales::dollar_format(),w.pre="$")})
     output$table_msa_income <- renderDataTable({create.clean.tbl(yr=latest.yr, g.type=c("MSA"), c.name="Median income", t.container=create.custom.container("MSA","Median Income"), t.cols=msa.tot.cols, s.cols=msa.shr.cols, e.type=input$IncomeType, w.pre="$")})
     
     output$chart_county_income <- renderPlotly({create.bar.chart.facet(yr=latest.yr, g.type=c("County"), e.type=input$IncomeType, c.name="Median income",w.label=scales::dollar_format(),w.pre="$")})
